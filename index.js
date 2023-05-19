fafd1484b8c38e4066d0ec02ee1894ea00edd690
Kristina Stout <kristinalstout@gmail.com>
2023-05-19 01:01:31 -0400

    created employee functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+const employee = {
+        name : "Name",
+        address : "address",
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return{
+        ...employee,
+        [key]:value,
+    };
+};
+updateEmployeeWithKeyAndValue(employee,"name","Sam");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]=value;
+    return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee,"name","Sam");
+
+function deleteFromEmployeeByKey(employee,key){
+    let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
